f33fd8b3db434f7bc36ae843b90a1cb244b8d706
NiamatUllah9410 <niamatkhan9410@gmail.com>
2025-03-13 02:39:25 +0500

    created-how-it-works page

---
+++ src/pages/HowItWorks.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../components/Banner'
+
+const HowItWorks = () => {
+  return (
+    <main>
+        <Banner />
+    </main>
+  )
+}
+
+export default HowItWorks
